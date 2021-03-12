7f841ff3956e53d138ce2db4a5dff48c51bc0306
shun2015 <dwat.pmjg@gmail.com>
2021-03-12 18:21:57 +0000

    update layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  
   def new
     @post = Post.new
   end
@@ -7,7 +8,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to posts_path
+    redirect_to post_path(@post)
   end
 
   def index
@@ -38,8 +39,11 @@ class PostsController < ApplicationController
   
   def destroy
     @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to posts_path
+    if @post.destroy
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   private
@@ -47,4 +51,5 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :date, :score, :impression, post_images_images: [])
   end
+  
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,14 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  
-  def enum_i18n(enum_name)
-    return nil if self.send(enum_name).nil?
-    I18n.t!("enums.#{self.model_name.i18n_key}.#{enum_name}.#{self.send(enum_name)}")
-  end
-
-  def self.enums_i18n(enum_name)
-    self.send(enum_name.to_s.pluralize).map do |key, value|
-      [I18n.t!("enums.#{self.model_name.i18n_key}.#{enum_name}.#{key}"), value]
-    end.to_h
-  end
 end
