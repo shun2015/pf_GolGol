cc79a1cf2e45b03dcb04085323d6f74408d4b7b5
shun2015 <dwat.pmjg@gmail.com>
2021-03-27 05:25:59 +0000

    deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:top]
-
+  before_action :authenticate_user!, except: [:top] #ログインしていないユーザーはtop画面のみ閲覧可能
+  
+  #新規登録後の遷移先
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
   end
